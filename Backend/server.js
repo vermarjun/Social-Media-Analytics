32f4a97f801382c61481962fe3dccd7454f7c3f8
vermarjun <vermarjun26@gmail.com>
2025-01-10 17:14:03 +0530

    _

--- Backend/server.js
+++ Backend/server.js
@@ -85,12 +85,13 @@ class LangflowClient {
 }
 
 const app = express();
-const corsOptions ={
-    origin:'*', 
-    credentials:true,            //access-control-allow-credentials:true
-    optionSuccessStatus:200,
- }
- app.use(cors(corsOptions)) 
+// const corsOptions ={
+//     origin:'*', 
+//     credentials:true,            //access-control-allow-credentials:true
+//     optionSuccessStatus:200,
+//  }
+//  app.use(cors(corsOptions)) 
+app.use(cors());
 app.use(bodyParser.json());
 
 const PORT = 3000 || process.env.PORT;
